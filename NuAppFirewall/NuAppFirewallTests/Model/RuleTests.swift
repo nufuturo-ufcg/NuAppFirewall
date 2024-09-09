2501d16211662458f5b03db63699b0e74979b3ee
Maya Brito <may20brito@gmail.com>
2024-09-09 14:59:49 +0000

    Adding implementation of the equals method

--- NuAppFirewall/NuAppFirewallTests/Model/RuleTests.swift
+++ NuAppFirewall/NuAppFirewallTests/Model/RuleTests.swift
@@ -15,11 +15,11 @@ class RuleTests: XCTestCase {
     override func setUpWithError() throws {
         super.setUp()
     }
-
+    
     override func tearDownWithError()throws {
         super.tearDown()
     }
-
+    
     // Tests the initialization of a Rule object with valid data
     func testRuleInitialization() throws {
         let ruleID = "/Applications/SampleApp.app"
@@ -37,7 +37,7 @@ class RuleTests: XCTestCase {
         XCTAssertEqual(rule?.destinations, destinations)
         XCTAssertEqual(rule?.direction, direction)
     }
-
+    
     // Tests the description method to ensure the output is formatted correctly
     func testRuleDescription() throws {
         let ruleID = "/Applications/BlockedApp.app"
@@ -48,7 +48,7 @@ class RuleTests: XCTestCase {
         let rule = Rule(ruleID: ruleID, action: action, appLocation: appLocation, destinations: destinations, direction: direction)
         
         let description = rule?.description()
-
+        
         let expectedDescription = """
         ruleID: /Applications/BlockedApp.app
         Action: allow
@@ -58,7 +58,7 @@ class RuleTests: XCTestCase {
         """
         XCTAssertEqual(description, expectedDescription)
     }
-
+    
     // Tests the creation of a Rule with an empty destinations set
     func testRuleWithEmptyDestinations() throws {
         let ruleID = "/Applications/SampleApp.app"
@@ -66,23 +66,45 @@ class RuleTests: XCTestCase {
         let appLocation = "/Applications/SampleApp.app"
         let destinations: Set<String> = []
         let direction = "outgoing"
-
+        
         let rule = Rule(ruleID: ruleID, action: action, appLocation: appLocation, destinations: destinations, direction: direction)
-
+        
         XCTAssertNil(rule)
     }
-
+    
     // Tests the creation of a Rule with non-empty destinations
     func testRuleWithNonEmptyDestinations() throws {
         let ruleID = "67890"
         let action = "allow"
         let appLocation = "/Applications/AnotherApp.app"
         let destinations: Set<String> = ["8.8.8.8"]
-        let direction = "incoming"
+        let direction = "outgoing"
         
         let rule = Rule(ruleID: ruleID, action: action, appLocation: appLocation, destinations: destinations, direction: direction)
-
-        XCTAssertNotNil(rule) 
+        
+        XCTAssertNotNil(rule)
         XCTAssertEqual(rule?.destinations, destinations)
+    }
+    
+    // Test the equals method (Equatable implementation)
+    func testRuleEquality() throws {
+        // First rule instance
+        let ruleID = "/Applications/TestApp.app"
+        let action = "allow"
+        let appLocation = "/Applications/TestApp.app"
+        let destinations: Set<String> = ["192.168.1.1"]
+        let direction = "outgoing"
+        
+        let rule1 = Rule(ruleID: ruleID, action: action, appLocation: appLocation, destinations: destinations, direction: direction)
+        
+        let rule2 = Rule(ruleID: ruleID, action: action, appLocation: appLocation, destinations: destinations, direction: direction)
+        
+        let rule3 = Rule(ruleID: "/Applications/DifferentApp.app", action: action, appLocation: "/Applications/DifferentApp.app", destinations: ["10.0.0.1"], direction: direction)
+        
+        // Ensure rule1 and rule2 are equal
+        XCTAssertEqual(rule1, rule2, "Rules with the same properties should be considered equal")
+        
+        // Ensure rule1 and rule3 are not equal
+        XCTAssertNotEqual(rule1, rule3, "Rules with different properties should not be considered equal")
     }
 }
