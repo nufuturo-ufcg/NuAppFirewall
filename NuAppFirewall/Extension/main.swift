ee8a7591b4d2bfa0ab770a91c1f82a19dd1d7aad
ec2-user <ec2-user@ip-172-31-44-150.ec2.internal>
2024-09-16 19:35:56 +0000

    adicionando os logs

--- NuAppFirewall/Extension/main.swift
+++ NuAppFirewall/Extension/main.swift
@@ -7,8 +7,12 @@
 
 import Foundation
 import NetworkExtension
+import OSLog
+
+let filterlogger = Logger(subsystem: "com.nufuturo.nuappfirewall.extension", category: "extension");
 
 autoreleasepool {
+    filterlogger.log("System extension mode was called")
     NEProvider.startSystemExtensionMode()
 }
 
