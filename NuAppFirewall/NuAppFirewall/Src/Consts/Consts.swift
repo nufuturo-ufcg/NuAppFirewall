7d5e611cd8461fd4701ce19ca1f4b6dadf23a634
carolmfreitas <carolina.freitas@ccc.ufcg.edu.br>
2024-10-23 12:48:13 +0000

    adiciona file path em consts

--- NuAppFirewall/NuAppFirewall/Src/Consts/Consts.swift
+++ NuAppFirewall/NuAppFirewall/Src/Consts/Consts.swift
@@ -13,5 +13,16 @@ struct Consts {
     static let verdictAllow = "allow"
     static let verdictBlock = "block"
     static let NoneString = "None"
+    
+    static let filePath: URL? = {
+        let fileManager = FileManager.default
+        
+        guard let containerURL = fileManager.containerURL(forSecurityApplicationGroupIdentifier: "27XB45N6Y5.com.nufuturo.nuappfirewall") else {
+            LogManager.logManager.log("Unable to find container URL")
+            return nil
+        }
+        
+        return containerURL
+    }()
 }
 
