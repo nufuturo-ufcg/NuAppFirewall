62fc1c24dd53f8895e009ff7c55f3b694b634f98
walber-araujo <walber.wesley.felix.araujo.filho@ccc.ufcg.edu.br>
2024-11-05 13:13:11 -0300

    Adiciona constante unknown, adiciona porta no log e refatora a verificação de regras

--- NuAppFirewall/NuAppFirewall/Src/Controller/FlowManager.swift
+++ NuAppFirewall/NuAppFirewall/Src/Controller/FlowManager.swift
@@ -18,28 +18,27 @@ public class FlowManager {
     }
     
     func handleNewFlow(_ flow: NEFilterFlow) -> NEFilterNewFlowVerdict {
-        
         LogManager.logManager.log("handling new flow in FlowManager", level: .debug, functionName: #function)
         
-        let (flowID, endpoint, url, subdomain, port, auditToken) = extractFlowInfo(from: flow)
+        let (flowID, endpoint, url, host, port, auditToken) = extractFlowInfo(from: flow)
         let pid = pidFromAuditToken(auditToken)
         let path = pathForProcess(with: pid)
         
-        LogManager.logManager.log("New flow SUBDOMAIN: \(subdomain)")
         LogManager.logManager.log("New flow URL: \(url)")
+        LogManager.logManager.log("New flow HOST: \(host)")
         LogManager.logManager.log("New flow PATH: \(path)")
         LogManager.logManager.log("New flow PORT: \(port)")
         
-        if let rule = rulesManager.getRule(appPath: path, endpoint: subdomain, port: port) {
+        if let rule = rulesManager.getRule(appPath: path, url: url, host: host, ip: endpoint, port: port) {
             let verdict: NEFilterNewFlowVerdict = rule.action == Consts.verdictBlock ? .drop() : .allow()
             let actionVerdict = rule.action == Consts.verdictBlock ? Consts.verdictBlock : Consts.verdictAllow
             
             LogManager.logManager.log(rule.description())
-            LogManager.logManager.logNewFlow(category: Consts.categoryConnection, flowID: flowID, auditToken: auditToken, endpoint: endpoint,mode: Consts.modePassive, url: url, verdict: actionVerdict, process: path, ruleID: rule.ruleID)
+            LogManager.logManager.logNewFlow(category: Consts.categoryConnection, flowID: flowID, auditToken: auditToken, endpoint: endpoint, port: port, mode: Consts.modePassive, url: url, verdict: actionVerdict, process: path, ruleID: rule.ruleID)
             
             return verdict
         } else {
-            LogManager.logManager.logNewFlow(category: Consts.categoryConnection, flowID: flowID, auditToken: auditToken, endpoint: endpoint, mode: Consts.modePassive, url: url, verdict: Consts.verdictAllow, process: path, ruleID: Consts.NoneString)
+            LogManager.logManager.logNewFlow(category: Consts.categoryConnection, flowID: flowID, auditToken: auditToken, endpoint: endpoint, port: port, mode: Consts.modePassive, url: url, verdict: Consts.verdictAllow, process: path, ruleID: Consts.NoneString)
             
             return NEFilterNewFlowVerdict.allow();
         }
@@ -50,8 +49,8 @@ public class FlowManager {
         
         let flowID = flow.identifier
         
-        var endpoint = "unknown"
-        var port = "unknown"
+        var endpoint = Consts.unknown
+        var port = Consts.unknown
         var auditToken = audit_token_t()
         
         if let socketFlow = flow as? NEFilterSocketFlow {
@@ -67,10 +66,10 @@ public class FlowManager {
             }
         }
         
-        let url = flow.url?.absoluteString ?? "unknown"
-        let subdomain = flow.url?.host(percentEncoded: true) ?? "unknown"
+        let url = flow.url?.absoluteString ?? Consts.unknown
+        let host = flow.url?.host(percentEncoded: true) ?? Consts.unknown
         
-        return (flowID, endpoint, url, subdomain, port, auditToken)
+        return (flowID, endpoint, url, host, port, auditToken)
     }
     
     func pidFromAuditToken(_ auditToken: audit_token_t) -> pid_t {
@@ -86,7 +85,7 @@ public class FlowManager {
         if result > 0 {
             return String(cString: buffer)
         } else {
-            return "unknown"
+            return Consts.unknown
         }
     }
 }
