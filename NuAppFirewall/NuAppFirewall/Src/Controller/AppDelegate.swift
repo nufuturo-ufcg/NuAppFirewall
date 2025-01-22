b91ceb47308e9de5aefdc41cc6178ec149be86b7
José Truta <jose.bomfim.truta.neto@ccc.ufcg.edu.br>
2025-01-22 10:51:05 -0300

    Criação de View para Popover e persistência de execução do App

--- NuAppFirewall/NuAppFirewall/Src/Controller/AppDelegate.swift
+++ NuAppFirewall/NuAppFirewall/Src/Controller/AppDelegate.swift
@@ -1,19 +1,11 @@
 import AppKit
 import SwiftUI
-import Cocoa
-
-extension NSImage.Name {
-    static let menuAppIcon = NSImage.Name("MenuAppIcon")
-}
 
 @main
 class MainApp: NSObject, NSApplicationDelegate {
-
-    @objc func statusBarButtonClicked(_ sender: NSStatusBarButton) {
-        print("menu item clicked")
-    }
     
     var statusBarItem: NSStatusItem!
+    var popover: NSPopover!
     
     func applicationDidFinishLaunching(_ notification: Notification) {
         print("sending request")
@@ -30,14 +22,45 @@ class MainApp: NSObject, NSApplicationDelegate {
             print("sysext is active and this is main thread")
             }
         
-        statusBarItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
+        self.setupStatusBarItem()
+        self.setupPopover()
+    }
+    
+    func applicationDidBecomeActive(_ notification: Notification) {
+        self.showPopover(self.statusBarItem.button!)
+    }
+    
+    private func setupStatusBarItem() {
+        self.statusBarItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
         
         if let button = statusBarItem.button {
-            button.action = #selector(statusBarButtonClicked(_:))
+            button.action = #selector(showPopover(_:))
             button.target = self
             button.image = NSImage(named: .menuAppIcon)
+            button.toolTip = "NuAppFirewall"
         } else {
             print("failed to create status bar button")
+        }
+    }
+    
+    private func setupPopover() {
+        let contentView = PopoverWindow()
+        
+        let popover = NSPopover()
+        popover.contentSize = NSSize(width: 350, height: 350)
+        popover.behavior = .transient
+        popover.contentViewController = NSHostingController(rootView: contentView)
+        self.popover = popover
+    }
+    
+    @objc func showPopover(_ sender: NSStatusBarButton) {
+        if let button = self.statusBarItem.button {
+            if self.popover.isShown {
+                self.popover.performClose(sender)
+            } else {
+                self.popover.show(relativeTo: button.bounds, of: button, preferredEdge: NSRectEdge.minY)
+                self.popover.contentViewController?.view.window?.makeKey()
+            }
         }
     }
     
@@ -48,5 +71,9 @@ class MainApp: NSObject, NSApplicationDelegate {
         app.delegate = delegate
         app.run()
     }
+}
+
+extension NSImage.Name {
+    static let menuAppIcon = NSImage.Name("MenuAppIcon")
 }
 
